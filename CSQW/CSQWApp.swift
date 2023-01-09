02b3b3f7c8954efa20db3cc1a9c50ec02ab0659a
rangerboy574 <jdeweese574@gmail.com>
2023-01-08 19:23:56 -0600

    Initial Commit

---
+++ CSQW/CSQWApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSQWApp.swift
+//  CSQW
+//
+//  Created by Joseph  DeWeese on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CSQWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
